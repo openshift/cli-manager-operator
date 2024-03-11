615d5fd474a34f02f66313e08f2c68e04b67ee96
Arda Güçlü <aguclu@redhat.com>
2024-03-11 14:23:46 +0300

    Initialize go mod and add plugin type api

---
+++ pkg/apis/plugin/v1alpha1/doc.go
@@ -0,0 +1,7 @@
+// Package v1alpha1 contains API Schema definitions for the plugin v1alpha1 API group
+// +k8s:defaulter-gen=TypeMeta
+// +k8s:deepcopy-gen=package,register
+// +groupName=config.openshift.io
+// +groupGoName=Plugins
+// +kubebuilder:validation:Optional
+package v1alpha1
