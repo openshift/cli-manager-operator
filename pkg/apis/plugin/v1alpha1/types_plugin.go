c5250771ca205e609f7d566e67f021c388551bfe
Arda Güçlü <aguclu@redhat.com>
2024-03-12 09:27:42 +0300

    Generate CRD specific code and API

--- pkg/apis/plugin/v1alpha1/types_plugin.go
+++ pkg/apis/plugin/v1alpha1/types_plugin.go
@@ -7,26 +7,32 @@ import (
 // PluginSpec defines the desired state of Plugin
 type PluginSpec struct {
 	// ShortDescription of the plugin.
+	// +kubebuilder:validation:Required
 	// +required
 	ShortDescription string `json:"shortDescription"`
 
 	// Description of the plugin.
+	// +kubebuilder:validation:Optional
 	// +optional
 	Description string `json:"description,omitempty"`
 
 	// Caveats of using the plugin.
+	// +kubebuilder:validation:Optional
 	// +optional
 	Caveats string `json:"caveats,omitempty"`
 
 	// Homepage of the plugin.
+	// +kubebuilder:validation:Optional
 	// +optional
 	Homepage string `json:"homepage,omitempty"`
 
 	// Version of the plugin.
+	// +kubebuilder:validation:Required
 	// +required
 	Version string `json:"version"`
 
 	// Platforms the plugin supports.
+	// +kubebuilder:validation:Required
 	// +required
 	Platforms []PluginPlatform `json:"platforms"`
 }
@@ -34,24 +40,29 @@ type PluginSpec struct {
 // PluginPlatform defines per-OS and per-Arch binaries for the given plugin.
 type PluginPlatform struct {
 	// Platform for the given binary (i.e. linux/amd64, darwin/amd64, windows/amd64).
+	// +kubebuilder:validation:Required
 	// +required
 	Platform string `json:"platform"`
 
 	// Image containing plugin.
+	// +kubebuilder:validation:Required
 	// +required
 	Image string `json:"image"`
 
 	// ImagePullSecret to use when connecting to an image registry that requires authentication.
+	// +kubebuilder:validation:Optional
 	// +optional
 	ImagePullSecret string `json:"imagePullSecret,omitempty"`
 
 	// Files is a list of file locations within the image that need to be extracted.
+	// +kubebuilder:validation:Required
 	// +required
 	Files []FileLocation `json:"files"`
 
 	// Bin specifies the path to the plugin executable.
 	// The path is relative to the root of the installation folder.
 	// The binary will be linked after all FileOperations are executed.
+	// +kubebuilder:validation:Required
 	// +required
 	Bin string `json:"bin"`
 }
@@ -61,10 +72,12 @@ type PluginPlatform struct {
 type FileLocation struct {
 	// From is the absolute file path within the image to copy from.
 	// Directories and wildcards are not currently supported.
+	// +kubebuilder:validation:Required
 	// +required
 	From string `json:"from"`
 
 	// To is the relative path within the root of the installation folder to place the file.
+	// +kubebuilder:validation:Required
 	// +required
 	To string `json:"to"`
 }
@@ -72,9 +85,12 @@ type FileLocation struct {
 // PluginStatus defines the observed state of Plugin.
 type PluginStatus struct{}
 
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
-//+kubebuilder:resource:path=Plugins,scope=Cluster
+//+kubebuilder:resource:scope=Cluster
 
 // Plugin is the Schema for the plugins API
 type Plugin struct {
@@ -85,6 +101,7 @@ type Plugin struct {
 	Status PluginStatus `json:"status,omitempty"`
 }
 
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 //+kubebuilder:object:root=true
 
 // PluginList contains a list of Plugin
